9a8ca23943897a0992eeb1be8aa66f984a3cf36e
Mikołaj Grzyb <mikolaj@codequest.com>
2016-10-18 15:42:02 +0200

    Remove immediate factory invocation.

--- Sortable.js
+++ Sortable.js
@@ -12,14 +12,14 @@
 		define(factory);
 	}
 	else if (typeof module != "undefined" && typeof module.exports != "undefined") {
-		module.exports = factory();
+		module.exports = factory;
 	}
 	else if (typeof Package !== "undefined") {
-		Sortable = factory();  // export for Meteor.js
+		Sortable = factory;  // export for Meteor.js
 	}
 	else {
 		/* jshint sub:true */
-		window["Sortable"] = factory();
+		window["Sortable"] = factory;
 	}
 })(function () {
 	"use strict";
